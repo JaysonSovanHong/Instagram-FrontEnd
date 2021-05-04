8211e39bfee6ac0eb8fe4198ab71439c208d7182
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-05-04 16:41:08 -0400

    updated

---
+++ instagram/src/components/ImagePost.js
@@ -0,0 +1,69 @@
+
+import axios from "axios"
+import { useState } from "react"
+import {useDropzone} from 'react-dropzone'
+
+const ImagePost = () => {
+    const [files, setFiles] = useState([])
+    const [image, setImage] = useState('') 
+    const [description, setDescription] = useState('')
+    
+   
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const formData = new FormData();
+            formData.append('description', description); 
+            formData.append('image',files[0])
+            console.log(formData);
+            const newPost = await axios.post(`${process.env.REACT_APP_BURL}/create`,formData, {headers:{authorization:localStorage.getItem("userId")}})
+            alert('upload complete')
+            console.log(newPost);
+        } catch (error) {
+            console.log({error});
+        }
+    }
+    
+    
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/*",
+        onDrop: (acceptedFiles) => {
+            setFiles(
+                acceptedFiles.map((file) =>
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                }))
+            )
+        },
+    })
+    console.log(files[0]);
+    
+    return(
+    <div>
+        
+       <form onSubmit={handleSubmit}>
+           <label htmlFor="upload"></label>
+            <div {...getRootProps()}>
+                upload
+            
+            </div>
+            <div>{files.map((file) => (
+            <div key={file.name}>
+            {console.log(file)}
+            <div>
+            <img src={file.preview} style={{ width: "200px" }} alt="preview" />
+            </div>
+                </div>
+                ))}
+            </div>
+            <label htmlFor='description'></label>
+            <input placeholder='description' id='description' value={description} onChange={(e) => setDescription(e.target.value)}  />
+            <button>Upload</button>
+            </form>
+                
+    </div>
+    )
+}
+
+export default ImagePost
--- instagram/src/pages/Main.js
+++ instagram/src/pages/Main.js
@@ -1,46 +1,21 @@
 import {UserContext} from '../Context/UserContext'
-import { useContext, useState } from "react"
-import {useDropzone} from 'react-dropzone'
+import { useContext } from "react"
+
 
 import Header from '../components/Header'
 import Timeline from '../components/Timeline'
 import Sidebar from '../components/Sidebar'
+import ImagePost from '../components/ImagePost'
 
 const Main = ({posts}) =>{
     const {userState} = useContext(UserContext) 
     const [user, setUser] = userState
-    const [files, setFiles] = useState([])
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: "image/*",
-        onDrop: (acceptedFiles) => {
-            setFiles(
-                acceptedFiles.map((file) =>
-                Object.assign(file, {
-                    preview: URL.createObjectURL(file),
-                })
-                )
-                )
-            },
-        })
-        const images = files.map((file) => (
-            <div key={file.name}>
-                <div>
-                    <img src={file.preview} style={{ width: "200px" }} alt="preview" />
-                </div>
-            </div>
-    ))
-
+    
     return(
     <div>
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-                <p>Post and Image</p>
-        </div>
-        
-        <div>{images}</div>   
-            
         <div className='container'> 
             <Header />
+            <ImagePost />
         </div>
         
         <div className='grid'>
